f8879e15c717bd5d31dd2a23fd19e35307f0ff63
M. Mert Yildiran <me@mertyildiran.com>
2022-12-02 06:00:04 +0300

    :fire: Remove `OASConfig` and `ServiceMap` from the config

--- pkg/models/config.go
+++ pkg/models/config.go
@@ -9,11 +9,6 @@ type Resources struct {
 	MemoryRequests string `yaml:"memory-requests" default:"50Mi"`
 }
 
-type OASConfig struct {
-	Enable        bool `yaml:"enabled" default:"true"`
-	MaxExampleLen int  `yaml:"max-example-len" default:"10240"`
-}
-
 type Config struct {
 	MaxDBSizeBytes     int64         `json:"maxDBSizeBytes"`
 	InsertionFilter    string        `json:"insertionFilter"`
@@ -22,6 +17,4 @@ type Config struct {
 	WorkerResources    Resources     `json:"workerResources"`
 	ResourcesNamespace string        `json:"resourceNamespace"`
 	DatabasePath       string        `json:"databasePath"`
-	ServiceMap         bool          `json:"serviceMap"`
-	OAS                OASConfig     `json:"oas"`
 }
